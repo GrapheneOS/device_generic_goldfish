84a59f965e69afd90199668b2ba60431e7be3728
Miroslav Tisma <miroslav.tisma@imgtec.com>
2015-09-23 12:54:56 +0200

    GPU: Build gralloc.ranchu.so
    Change-Id: Ie449043b06b6fefa52bdbd68c243a1165ec95ee1

--- opengl/system/gralloc/Android.mk
+++ opengl/system/gralloc/Android.mk
@@ -17,4 +17,19 @@ LOCAL_SHARED_LIBRARIES += libdl
 
 $(call emugl-end-module)
 
+$(call emugl-begin-shared-library,gralloc.ranchu)
+$(call emugl-import,libGLESv1_enc lib_renderControl_enc libOpenglSystemCommon)
+$(call emugl-set-shared-library-subpath,hw)
+
+LOCAL_CFLAGS += -DLOG_TAG=\"gralloc_ranchu\"
+LOCAL_CFLAGS += -Wno-missing-field-initializers
+
+LOCAL_SRC_FILES := gralloc.cpp
+
+# Need to access the special OPENGL TLS Slot
+LOCAL_C_INCLUDES += bionic/libc/private
+LOCAL_SHARED_LIBRARIES += libdl
+
+$(call emugl-end-module)
+
 endif # BUILD_EMULATOR_OPENGL_DRIVER != false
