fc6db8d7a61030d90437343cb6fea05b15213c6b
Yahan Zhou <yahan@google.com>
2017-09-14 15:47:05 -0700

    Fix camera orientation
    BUG: 65365207
    
    Test: manual test cts-verifier Camera FOV Calibration
    Change-Id: Ieef8807c211b3e07ce643109a0978130532a0cb6

--- camera/EmulatedCameraFactory.h
+++ camera/EmulatedCameraFactory.h
@@ -150,7 +150,7 @@ public:
      */
     int getFakeCameraOrientation() {
         // TODO: Have a boot property that controls that.
-        return 0;
+        return 90;
     }
 
     /*
@@ -158,7 +158,7 @@ public:
      */
     int getQemuCameraOrientation() {
         // TODO: Have a boot property that controls that.
-        return 0;
+        return 90;
     }
 
     /*
