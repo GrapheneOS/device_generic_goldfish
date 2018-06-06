aa95367f9f97df374dbd2c174459c24d92e210e1
bohu <bohu@google.com>
2018-06-06 15:00:41 -0700

    goldfish: enable -no-boot-anim
    It turns out vendor-init cannot read ro.kernel.android.bootanim
    property, so this cl does a translation of that property
    into vendor.qemu space.
    
    BUG: 109831748
    Change-Id: I9b1b61d6a4ac8098a96ffbbb67699b2e502dc0ca

--- init.ranchu-core.sh
+++ init.ranchu-core.sh
@@ -1,4 +1,14 @@
 #!/vendor/bin/sh
 
+#init cannot access ro.kernel.android.bootanim,
+#so do a translation into vendor.qemu space
+bootanim=`getprop ro.kernel.android.bootanim`
+case "$bootanim" in
+    "")
+    ;;
+    *) setprop vendor.qemu.android.bootanim 0
+    ;;
+esac
+
 # take the wake lock
 echo "emulator_wake_lock" > /sys/power/wake_lock
