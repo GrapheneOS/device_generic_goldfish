6d5cffec820e39aacd50bca5600222e22d9bf9f9
Roman Kiryanov <rkir@google.com>
2018-11-05 11:43:40 -0800

    Switch makefiles back to specify the kernel version explicitly
    Ninja does not support symlinks well: there is no way to
    get symlink's modification time (when switching versions)
    to calculate which targets needs to be updated.
    
    Bug: 117438872
    Test: make -j
    Change-Id: I8fce3eb9fc8b771045c5b5a129b55fcb9a4053e4
    Signed-off-by: Roman Kiryanov <rkir@google.com>

--- x86_64-vendor.mk
+++ x86_64-vendor.mk
@@ -1,3 +1,4 @@
+PRODUCT_KERNEL_VERSION := 4.14
 
 PRODUCT_PROPERTY_OVERRIDES += \
        vendor.rild.libpath=/vendor/lib64/libgoldfish-ril.so
@@ -9,7 +10,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     development/sys-img/advancedFeatures.ini:advancedFeatures.ini \
     device/generic/goldfish/data/etc/encryptionkey.img:encryptionkey.img \
-    prebuilts/qemu-kernel/x86_64/current/kernel-qemu2:kernel-ranchu
+    prebuilts/qemu-kernel/x86_64/$(PRODUCT_KERNEL_VERSION)/kernel-qemu2:kernel-ranchu
 
 PRODUCT_SHIPPING_API_LEVEL := 28
 TARGET_USES_MKE2FS := true
