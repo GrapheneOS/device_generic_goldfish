63ba389337e8706973f3b634e91780229738335f
Roman Kiryanov <rkir@google.com>
2019-03-08 00:37:23 +0000

    Revert "goldfish_kernel_tests_x86_64: Make it to boot further"
    This reverts commit 14f0f70b5d71273eb06605f3ecd6951ba1d24fa4.
    
    Reason for revert: the build is broken
    
    https://android-build.googleplex.com/builds/submitted/5359397/build_test/latest/view/logs/build_error.log
    
    Change-Id: I901de3a372237681fbc814d489088f7ee9158588

--- kernel-tests/goldfish_kernel_tests_x86_64.mk
+++ kernel-tests/goldfish_kernel_tests_x86_64.mk
@@ -22,15 +22,11 @@ BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 512
 BOARD_HAVE_BLUETOOTH := false
 
-SYSTEM_CORE_ROOTDIR := system/core/rootdir
-PREBUILTS := prebuilts
-GOLDFISH_DIR := device/generic/goldfish
 THIS_DIR := device/generic/goldfish/kernel-tests
 
 # android.hardware.
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-service \
-    android.hardware.keymaster@3.0-impl \
     android.hardware.drm@1.1-service.clearkey \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.allocator@2.0-impl \
@@ -54,6 +50,7 @@ PRODUCT_PACKAGES += \
     libdl \
     libutils \
     libsysutils \
+    libbinder \
     libhardware \
     libhardware_legacy \
     linker \
@@ -84,11 +81,18 @@ PRODUCT_HOST_PACKAGES += \
 PRODUCT_PACKAGES += \
     qemu-props \
 
+# Graphics
+PRODUCT_PACKAGES += \
+
 # Device modules
 PRODUCT_PACKAGES += \
     android.system.suspend@1.0-service \
     ashmemd \
     libashmemd_client \
+    keymaster@3.0-impl \
+    gralloc.goldfish \
+    gralloc.goldfish.default \
+    gralloc.ranchu \
     servicemanager \
     hwservicemanager \
     vndservice \
@@ -97,8 +101,6 @@ PRODUCT_PACKAGES += \
     toybox \
     vold \
     init \
-    init_system \
-    init_vendor \
     init.environ.rc \
     init.rc \
     reboot \
@@ -128,25 +130,14 @@ PRODUCT_HOST_PACKAGES += \
     selinux_policy_system \
 
 PRODUCT_COPY_FILES += \
-    $(SYSTEM_CORE_ROOTDIR)/init.usb.rc:root/init.usb.rc \
-    $(SYSTEM_CORE_ROOTDIR)/init.usb.configfs.rc:root/init.usb.configfs.rc \
-    $(SYSTEM_CORE_ROOTDIR)/ueventd.rc:root/ueventd.rc \
-    $(SYSTEM_CORE_ROOTDIR)/etc/hosts:system/etc/hosts \
-
-PRODUCT_COPY_FILES += \
-    $(PREBUILTS)/qemu-kernel/x86_64/4.14/kernel-qemu2:kernel-ranchu \
-
-PRODUCT_COPY_FILES += \
-    $(GOLDFISH_DIR)/data/etc/encryptionkey.img:encryptionkey.img \
-    $(GOLDFISH_DIR)/ueventd.ranchu.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc \
-
-PRODUCT_COPY_FILES += \
     $(THIS_DIR)/manifest.xml:$(TARGET_COPY_OUT_VENDOR)/manifest.xml \
     $(THIS_DIR)/init.ranchu-core.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.ranchu-core.sh \
     $(THIS_DIR)/init.ranchu.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.ranchu.rc \
     $(THIS_DIR)/fstab.ranchu:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.ranchu \
     $(THIS_DIR)/config.ini:config.ini \
-    $(THIS_DIR)/advancedFeatures.ini:advancedFeatures.ini \
+    device/generic/goldfish/data/etc/encryptionkey.img:encryptionkey.img \
+    development/sys-img/advancedFeatures.ini:advancedFeatures.ini \
+    prebuilts/qemu-kernel/x86_64/4.14/kernel-qemu2:kernel-ranchu \
 
 # The set of packages we want to force 'speed' compilation on.
 PRODUCT_DEXPREOPT_SPEED_APPS := \
