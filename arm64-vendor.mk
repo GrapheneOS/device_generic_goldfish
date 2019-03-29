29b048b527b20621f8d89d9d19110c7cf076f93d
Kenneth Magic <kmagic@google.com>
2019-03-29 11:50:08 -0700

    Add PRODUCT_SDKADDON_COPY_FILES to reduce duplication.
    This allows removing the same definitions in multiple files that
    inherit these.
    
    Bug: 128515898
    Test: Built / verified variants still contained files.
    Change-Id: Ie7a741d49ed4c1602d3a6e69c54543d9eada33e5
    (cherry picked from commit 4ecaeff77161bf15b9369613a6a33d2607a4ac05)

--- arm64-vendor.mk
+++ arm64-vendor.mk
@@ -6,11 +6,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Note: the following lines need to stay at the beginning so that it can
 # take priority  and override the rules it inherit from other mk files
 # see copy file rules in core/Makefile
+PRODUCT_SDK_ADDON_COPY_FILES += \
+    development/sys-img/advancedFeatures.ini.arm:images/arm64-v8a/advancedFeatures.ini \
+    prebuilts/qemu-kernel/arm64/$(PRODUCT_KERNEL_VERSION)/kernel-qemu2:images/arm64-v8a/kernel-ranchu
+
 PRODUCT_COPY_FILES += \
     device/generic/goldfish/manifest-arm.xml:$(TARGET_COPY_OUT_VENDOR)/manifest.xml \
     development/sys-img/advancedFeatures.ini.arm:advancedFeatures.ini \
     prebuilts/qemu-kernel/arm64/$(PRODUCT_KERNEL_VERSION)/kernel-qemu2:kernel-ranchu \
     device/generic/goldfish/fstab.ranchu.arm:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.ranchu
-
 EMULATOR_VENDOR_NO_GNSS := true
 
