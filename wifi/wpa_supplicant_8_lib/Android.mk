1483ba1a5759fe5d355c4437a40e6ed61884805b
Dan Willemsen <dwillemsen@google.com>
2019-05-28 13:29:58 -0700

    Remove reference to non-existant directory
    device/google/gce/include no longer exists in the manifest.
    
    Test: treehugger
    Change-Id: I82c76d7028f17b2edb11c5a763e0d9121c081b44

--- wifi/wpa_supplicant_8_lib/Android.mk
+++ wifi/wpa_supplicant_8_lib/Android.mk
@@ -74,7 +74,6 @@ LOCAL_CFLAGS := $(L_CFLAGS) \
 LOCAL_SRC_FILES := $(WPA_SRC_FILE)
 
 LOCAL_C_INCLUDES := \
-  device/google/gce/include \
   $(WPA_SUPPL_DIR_INCLUDE)\
 
 include $(BUILD_STATIC_LIBRARY)
