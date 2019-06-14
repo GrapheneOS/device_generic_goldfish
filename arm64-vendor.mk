1320f28fbbfa78d117471a2d93debac386bc800e
Janis Danisevskis <jdanis@google.com>
2019-06-14 15:41:27 -0700

    Fix goldfish manifest handling.
    Copying in the manifest file breaks vintf_fragment based manifest
    generation. This patch specifies the manifest.xml file using
    the build system variable rather then copying it over the generated
    file.
    
    Shout out to mmaurer@google.com for finding this.
    
    Test: Builds without conflict and produces the correct manifest.xml
    Bug: 135280519
    Change-Id: Icb9bae2336e11fe5b1fda532adbee500b2d8dbaf
    Merged-In: Icb9bae2336e11fe5b1fda532adbee500b2d8dbaf

--- arm64-vendor.mk
+++ arm64-vendor.mk
@@ -3,6 +3,8 @@ PRODUCT_KERNEL_VERSION := 4.4
 PRODUCT_PROPERTY_OVERRIDES += \
        vendor.rild.libpath=/vendor/lib64/libgoldfish-ril.so
 
+DEVICE_MANIFEST_FILE := device/generic/goldfish/manifest-arm.xml
+
 # Note: the following lines need to stay at the beginning so that it can
 # take priority  and override the rules it inherit from other mk files
 # see copy file rules in core/Makefile
@@ -12,7 +14,6 @@ PRODUCT_SDK_ADDON_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     device/generic/goldfish/fstab.ranchu.initrd.arm:$(TARGET_COPY_OUT_RAMDISK)/fstab.ranchu \
-    device/generic/goldfish/manifest-arm.xml:$(TARGET_COPY_OUT_VENDOR)/manifest.xml \
     device/generic/goldfish/data/etc/advancedFeatures.ini.arm:advancedFeatures.ini \
     prebuilts/qemu-kernel/arm64/$(PRODUCT_KERNEL_VERSION)/kernel-qemu2:kernel-ranchu \
     device/generic/goldfish/fstab.ranchu.arm:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.ranchu
