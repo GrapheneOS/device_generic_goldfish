b64da5cf7ac7b4dcacd4ebb7e1c772396e4b3d9b
Bo Hu <bohu@google.com>
2020-01-07 23:54:25 +0000

    Revert "Upgrade HWC from 2.1 to 2.3"
    This reverts commit 73d4fe7bf95d8a9a3d6ec873cf63f91afed8f09d.
    
    Reason for revert: b/147264301
    
    Change-Id: Icf6bbaa0e967e6c67f04fc546dae34f1e27ad4b7

--- vendor.mk
+++ vendor.mk
@@ -65,8 +65,8 @@ PRODUCT_PACKAGES += \
     power.goldfish \
     power.ranchu \
     fingerprint.ranchu \
-    android.hardware.graphics.composer@2.3-impl \
-    android.hardware.graphics.composer@2.3-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.mapper@2.0-impl \
