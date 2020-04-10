ab57af8cfa0d22495459c544301ece69f6a156f1
Roman Kiryanov <rkir@google.com>
2020-04-10 15:36:21 -0700

    Upgrade sensors to @2.0
    Bug: 153754380
    Test: atest VtsHalSensorsV2_0TargetTest
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Change-Id: Id942e8638bc36c90928c4ff3aba524443b4889a7

---
+++ sensors/util.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+namespace goldfish {
+
+int qemu_pipe_open_ns(const char* ns, const char* pipeName, int flags);
+int qemu_pipe_read_fully(int pipe, void* buffer, int len);
+int qemu_pipe_write_fully(int pipe, const void* buffer, int len);
+
+int qemud_channel_open(const char* name);
+int qemud_channel_send(int pipe, const void* msg, int msglen);
+int qemud_channel_recv(int pipe, void*  msg, int msgsize);
+
+}  // namespace goldfish
