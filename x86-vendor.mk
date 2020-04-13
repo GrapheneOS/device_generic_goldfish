d334602d2d425bf96d067a8a404b23ecbf64629b
Yo Chiang <yochiang@google.com>
2020-04-13 16:45:11 +0800

    Remove ELF PRODUCT_COPY_FILES
    We're banning ELF prebuilts in PRODUCT_COPY_FILES. Define prebuilt
    modules for vendor prebuilts so we can run check_elf_file.py on them.
    Use PRODUCT_PACKAGES to install ELF prebuilts.
    
    Bug: 140560012
    Test: Build pass. Built artifacts should not change.
    Change-Id: If3d63f00feaa689cbd7acdc175ad5ffc31136129

--- x86-vendor.mk
+++ x86-vendor.mk
@@ -20,8 +20,8 @@ PRODUCT_SDK_ADDON_COPY_FILES += \
     device/generic/goldfish/data/etc/encryptionkey.img:images/x86/encryptionkey.img \
     prebuilts/qemu-kernel/x86_64/$(PRODUCT_KERNEL_VERSION)/kernel-qemu2:images/x86/kernel-ranchu-64
 
-PRODUCT_COPY_FILES += \
-    device/generic/goldfish/data/etc/x86/emulatorip:$(TARGET_COPY_OUT_VENDOR)/bin/ip
+PRODUCT_PACKAGES += \
+    emulatorip
 
 
 PRODUCT_SHIPPING_API_LEVEL := 28
