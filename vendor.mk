ad6e9379aaa9866f74cbba44f0dfce36b68095b3
Diego Vela <diegovela@google.com>
2020-06-02 12:49:22 -0700

    Adjust emulator dependencies to use sidecar
    Replace androidx.window.extensions with androidx.window.sidecar
    
    Bug: 157477145
    Test: Local manual test.
    Change-Id: I8e18a4944608fd3f116ac9093c4802dfdcc3b938

--- vendor.mk
+++ vendor.mk
@@ -209,7 +209,7 @@ PRODUCT_PACKAGES += \
 
 # Extension implementation for Jetpack WindowManager
 PRODUCT_PACKAGES += \
-    androidx.window.extensions
+    androidx.window.sidecar
 
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.face@1.0-service.example
