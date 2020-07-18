ce5cfcfe5f2d0bcce364ee3cb956b448c261e8e8
Roman Kiryanov <rkir@google.com>
2020-07-18 15:43:43 -0700

    Use the default r_submix policy
    Since we use the default r_submix implementtaion
    let's use its default policy.
    
    Bug: 157158109
    Test: presubmit
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Change-Id: Ib63da43ccbc056d00fcdf276b03d9f3ede3cab5b
    Merged-In: Ib63da43ccbc056d00fcdf276b03d9f3ede3cab5b

--- vendor.mk
+++ vendor.mk
@@ -158,7 +158,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     device/generic/goldfish/audio/policy/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     device/generic/goldfish/audio/policy/primary_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/primary_audio_policy_configuration.xml \
-    device/generic/goldfish/audio/policy/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+
 endif
 
 PRODUCT_PACKAGES += \
