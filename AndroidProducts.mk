a7c348a2b2a9b23ae968bfb1bfc5da5629cc3ffc
bohu <bohu@google.com>
2020-12-30 10:12:55 -0800

    64bitonly: add sdk_phone64_x86_64
    also the emulator64_x86_64 board
    
    This serves as a start to migrate emulator
    configs out of the build/make
    
    Test
    
    lunch sdk_phone64_x86_64-userdebug
    emulator
    
    Change-Id: Iea0dd8005ca079020ebfdfabe1962ea214669408

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,5 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/sdk_phone_x86_vendor.mk \
+    $(LOCAL_DIR)/64bitonly/product/sdk_phone64_x86_64.mk \
     $(LOCAL_DIR)/fvp.mk \
     $(LOCAL_DIR)/fvp_mini.mk
