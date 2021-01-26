cb200b5c393a17b4c79dd58b557e9f26df9bbe53
bohu <bohu@google.com>
2021-01-25 22:32:14 -0800

    goldfish: put back the drm default service and impl
    They are still needed (e.g., by chrome); and the
    hardware/interfaces/drm should allow 64bit only
    build (another cl)
    
    Change-Id: I79fe2b64de34f416b1b12721827d8d7311dd960a

--- 64bitonly/product/vendor.mk
+++ 64bitonly/product/vendor.mk
@@ -120,6 +120,8 @@ PRODUCT_COPY_FILES += \
 endif
 
 PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.0-impl \
     android.hardware.drm@1.3-service.clearkey \
 
 PRODUCT_PACKAGES += \
