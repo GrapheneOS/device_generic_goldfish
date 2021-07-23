3ec885a53913f2a67962ec56f60557c8cfd71248
Ming-Shin Lu <lumark@google.com>
2021-07-23 22:37:53 +0800

    [Multi-Display emulator] add display settings configuration
    As Multi-Display emulator supports creating virtual displays from
    extanded panel > Displays settings.
    
    To support soft-keyboard can display on the target virtual display,
    add display_settings.xml to setup the IME display policy for the
    given system owned virtual display.
    
    Fix: 193078905
    Test: 1) Build and start the emulator,
          2) Attach a secondary display from Extended Panel .
          3) Launch Gmail on the secondary display and try entering
             Google Sign In Credentials, make sure the soft-keyboard
             can show on the target secondary display.
    Change-Id: Id82cd0735ec53c10019e348c412fb802ec715110

--- 64bitonly/product/vendor.mk
+++ 64bitonly/product/vendor.mk
@@ -292,6 +292,7 @@ PRODUCT_COPY_FILES += \
     device/generic/goldfish/input/virtio_input_multi_touch_10.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/virtio_input_multi_touch_10.idc \
     device/generic/goldfish/input/virtio_input_multi_touch_11.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/virtio_input_multi_touch_11.idc \
     device/generic/goldfish/display_settings_freeform.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display_settings_freeform.xml \
+    device/generic/goldfish/display_settings.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display_settings.xml \
     device/generic/goldfish/device_state_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/devicestate/device_state_configuration.xml \
     device/generic/goldfish/data/etc/config.ini:config.ini \
     device/generic/goldfish/wifi/simulated_hostapd.conf:$(TARGET_COPY_OUT_VENDOR)/etc/simulated_hostapd.conf \
