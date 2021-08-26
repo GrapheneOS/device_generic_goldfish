16cd1945ac7f8a9f198dec999bf14e2e2f07fa70
Roman Kiryanov <rkir@google.com>
2021-08-26 15:27:19 -0700

    Move audio.effects into a separate manifest
    gcar targets have different HAL version.
    
    Bug: 197888672
    Test: presubmit
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Change-Id: I308442366679280eb19b671acbb56dbfcf044b05
    Merged-In: I308442366679280eb19b671acbb56dbfcf044b05

--- vendor.mk
+++ vendor.mk
@@ -181,6 +181,8 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.2-impl.ranchu \
     android.hardware.audio.effect@7.0-impl \
 
+DEVICE_MANIFEST_FILE := device/generic/goldfish/audio/android.hardware.audio.effects@7.0.xml
+
 PRODUCT_COPY_FILES += \
     device/generic/goldfish/audio/policy/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     device/generic/goldfish/audio/policy/primary_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/primary_audio_policy_configuration.xml \
