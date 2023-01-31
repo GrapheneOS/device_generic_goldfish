2338f60a2d451950601d48f6ce2772f5dc516f88
Roman Kiryanov <rkir@google.com>
2023-01-31 09:49:30 -0800

    Support compressing YUV with arbitrary chroma_step
    `GraphicBufferMapper::lockYCbCr` might return arbitrary
    `chroma_step`.
    
    Bug: 251042358
    Test: presubmit
    Change-Id: I58dd51bd513c84355d9b5cb56f8484c09e01fff8
    Signed-off-by: Roman Kiryanov <rkir@google.com>

--- camera/yuv.cpp
+++ camera/yuv.cpp
@@ -23,6 +23,39 @@ namespace camera {
 namespace provider {
 namespace implementation {
 namespace yuv {
+namespace {
+
+void copyCbCrPlane(uint8_t* dst, const size_t width, size_t height,
+                   const void* src, const size_t srcStride, const size_t srcStep) {
+    const uint8_t* src8 = static_cast<const uint8_t*>(src);
+    for (; height > 0; --height, src8 += srcStride) {
+        const uint8_t* p = src8;
+        for (size_t rem = width & 15; rem; --rem, ++dst, p += srcStep) {
+            *dst = *p;
+        }
+
+        for (size_t width16 = width >> 4; width16; --width16) {
+            *dst = *p; ++dst; p += srcStep;
+            *dst = *p; ++dst; p += srcStep;
+            *dst = *p; ++dst; p += srcStep;
+            *dst = *p; ++dst; p += srcStep;
+            *dst = *p; ++dst; p += srcStep;
+            *dst = *p; ++dst; p += srcStep;
+            *dst = *p; ++dst; p += srcStep;
+            *dst = *p; ++dst; p += srcStep;
+            *dst = *p; ++dst; p += srcStep;
+            *dst = *p; ++dst; p += srcStep;
+            *dst = *p; ++dst; p += srcStep;
+            *dst = *p; ++dst; p += srcStep;
+            *dst = *p; ++dst; p += srcStep;
+            *dst = *p; ++dst; p += srcStep;
+            *dst = *p; ++dst; p += srcStep;
+            *dst = *p; ++dst; p += srcStep;
+        }
+    }
+}
+
+}  // namespace
 
 size_t NV21size(const size_t width, const size_t height) {
     LOG_ALWAYS_FATAL_IF((width & 1) || (height & 1));
@@ -44,6 +77,34 @@ android_ycbcr NV21init(const size_t width, const size_t height, void* data) {
 
     return nv21;
 }
+
+android_ycbcr toNV21Shallow(const size_t width, const size_t height,
+                            const android_ycbcr& ycbcr,
+                            std::vector<uint8_t>* data) {
+    LOG_ALWAYS_FATAL_IF((width & 1) || (height & 1));
+    if (ycbcr.chroma_step == 1) {
+        return ycbcr;
+    }
+
+    const size_t area = width * height;
+    data->resize(area / 2);  // only for CbCr
+
+    android_ycbcr nv21;
+    nv21.y = ycbcr.y;  // don't copy Y
+    nv21.ystride = ycbcr.ystride;
+    nv21.cb = &data[0];
+    nv21.cr = &data[area / 4];
+    nv21.cstride = width / 2;
+    nv21.chroma_step = 1;
+
+    copyCbCrPlane(&((*data)[0]), width / 2, height / 2,
+                  ycbcr.cb, ycbcr.cstride, ycbcr.chroma_step);
+    copyCbCrPlane(&((*data)[area / 4]), width / 2, height / 2,
+                  ycbcr.cr, ycbcr.cstride, ycbcr.chroma_step);
+
+    return nv21;
+}
+
 }  // namespace yuv
 }  // namespace implementation
 }  // namespace provider
