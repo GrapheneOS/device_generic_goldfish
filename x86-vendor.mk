c77ec4645f83c463e97ecae6ea5af7dc3066770f
Joshua Duong <joshuaduong@google.com>
2023-01-31 18:51:19 +0000

    sdk_gphone64_x86_64_minigbm target config.
    Overview:
    - includes minigbm gralloc (gralloc5) instead of goldfish gralloc
    - in advancedFeatures, include Minigbm feature, remove RefCountPipe
      feature
    
    Bug: 293603791
    Test: lunch sdk_gphone64_x86_64_minigbm
    
    Change-Id: I6a47a798dce372036e122ccf8c344e7a398e42f0
    Merged-In: I6a47a798dce372036e122ccf8c344e7a398e42f0

--- x86-vendor.mk
+++ x86-vendor.mk
@@ -1,5 +1,9 @@
 include device/generic/goldfish/x86_64-kernel.mk
 
+ADVANCED_FEATURES_FILE := advancedFeatures.ini
+ifneq ($(filter %_minigbm, $(TARGET_PRODUCT)),)
+ADVANCED_FEATURES_FILE := advancedFeatures.ini.minigbm
+endif
 PRODUCT_PROPERTY_OVERRIDES += \
        vendor.rild.libpath=/vendor/lib/libgoldfish-ril.so
 
@@ -9,12 +13,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # entirely appropriate to inherit from for on-device configurations.
 PRODUCT_COPY_FILES += \
     device/generic/goldfish/data/etc/config.ini.xl:config.ini \
-    device/generic/goldfish/data/etc/advancedFeatures.ini:advancedFeatures.ini \
+    device/generic/goldfish/data/etc/$(ADVANCED_FEATURES_FILE):advancedFeatures.ini \
     device/generic/goldfish/data/etc/encryptionkey.img:encryptionkey.img \
     $(EMULATOR_KERNEL_FILE):kernel-ranchu-64
 
 PRODUCT_SDK_ADDON_COPY_FILES += \
-    device/generic/goldfish/data/etc/advancedFeatures.ini:images/x86/advancedFeatures.ini \
+    device/generic/goldfish/data/etc/$(ADVANCED_FEATURES_FILE):images/x86/advancedFeatures.ini \
     device/generic/goldfish/data/etc/encryptionkey.img:images/x86/encryptionkey.img \
     $(EMULATOR_KERNEL_FILE):images/x86/kernel-ranchu-64
 
