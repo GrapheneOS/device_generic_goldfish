5da33a47d935a1e01994078573658d8724c48209
Roman Kiryanov <rkir@google.com>
2023-08-10 11:27:10 -0700

    Move PRODUCT_SHIPPING_API_LEVEL into vendor_common.mk
    to have it in one place
    
    Bug: 295259752
    Test: m
    Change-Id: I776e1a25c1faf5948376fd1815c59e3f484e4d7e
    Signed-off-by: Roman Kiryanov <rkir@google.com>

--- vendor_common.mk
+++ vendor_common.mk
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+PRODUCT_SHIPPING_API_LEVEL := 33
+
 #
 # This file is to configure vendor/data partitions of emulator-related products
 #
