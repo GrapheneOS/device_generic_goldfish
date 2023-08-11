2d7eaf4c22e9e203f1e595a1d606dbe0d983ce43
Roman Kiryanov <rkir@google.com>
2023-08-11 14:33:27 -0700

    Move PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS into kernel.mk files
    Bug: 295259752
    Test: presubmit
    Change-Id: I0fe057e6eca4d4f9e1aa49f96b12e6976b4fa31d
    Merged-In: I0fe057e6eca4d4f9e1aa49f96b12e6976b4fa31d
    Signed-off-by: Roman Kiryanov <rkir@google.com>

--- 64bitonly/product/arm64-vendor.mk
+++ 64bitonly/product/arm64-vendor.mk
@@ -7,7 +7,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.crypto.dm_default_key.options_format.version=2
 
 PRODUCT_SHIPPING_API_LEVEL := 34
-PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
 TARGET_USES_MKE2FS := true
 
 # Note: the following lines need to stay at the beginning so that it can
