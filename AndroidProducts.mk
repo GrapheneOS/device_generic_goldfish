16bd2f02c19d17086dbab406281cf35af2c51d44
Bo Hu <bohu@google.com>
2023-09-23 06:31:48 -0700

    fix phone and tablet targets
    phone targets will use less space
    tablets do not have telephony (most common cases)
    nor hinge (this is for foldable)
    
    Bug: 301464526
    Test: presubmit
    Change-Id: Ia63ac3d97be027617c7228283fb76f9fa56cff83
    Merged-In: Ia63ac3d97be027617c7228283fb76f9fa56cff83

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,6 +2,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/sdk_phone_x86_vendor.mk \
     $(LOCAL_DIR)/64bitonly/product/sdk_phone64_x86_64.mk \
     $(LOCAL_DIR)/64bitonly/product/sdk_phone64_x86_64_riscv64.mk \
+    $(LOCAL_DIR)/64bitonly/product/sdk_tablet_arm64.mk \
     $(LOCAL_DIR)/64bitonly/product/sdk_tablet_x86_64.mk \
     $(LOCAL_DIR)/64bitonly/product/sdk_phone64_arm64.mk \
     $(LOCAL_DIR)/64bitonly/product/sdk_slim_x86_64.mk \
