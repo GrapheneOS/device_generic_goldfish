690457da0f88419f37e12b21aae1c4ba52735e26
Roman Kiryanov <rkir@google.com>
2023-10-16 11:20:08 -0700

    Move PRODUCT_SHIPPING_API_LEVEL into vendor_common.mk
    to keep it assigned in one place.
    
    Bug: 295259752
    Test: presubmit
    Change-Id: I3afca5a03971d0aa1bc73e0e82e42d89bc6fa103
    Signed-off-by: Roman Kiryanov <rkir@google.com>

--- arm64-vendor.mk
+++ arm64-vendor.mk
@@ -6,7 +6,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.crypto.dm_default_key.options_format.version=2
 
-PRODUCT_SHIPPING_API_LEVEL := 34
 TARGET_USES_MKE2FS := true
 
 # Note: the following lines need to stay at the beginning so that it can
