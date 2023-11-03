3719fbd1b808a7f53fc1cd3760e3e93f963c7b8d
Roman Kiryanov <rkir@google.com>
2023-11-02 17:00:44 -0700

    Move the fvp stuff into its directory
    Bug: 295259752
    Test: presubmit
    Change-Id: I16f5418aea6c62927f782c55ab33245b1bcef955
    Signed-off-by: Roman Kiryanov <rkir@google.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,5 +6,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/64bitonly/product/sdk_phone64_arm64.mk \
     $(LOCAL_DIR)/64bitonly/product/sdk_slim_x86_64.mk \
     $(LOCAL_DIR)/64bitonly/product/sdk_slim_arm64.mk \
-    $(LOCAL_DIR)/fvp.mk \
-    $(LOCAL_DIR)/fvp_mini.mk
+    $(LOCAL_DIR)/fvpbase/fvp.mk \
+    $(LOCAL_DIR)/fvpbase/fvp_mini.mk
