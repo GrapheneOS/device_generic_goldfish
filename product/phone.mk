2697f46a4d6b4c7c0ed2b4210438b299d9e301f7
Roman Kiryanov <rkir@google.com>
2023-11-29 22:56:18 -0800

    Prepare to retire vendor.mk and emulator64_vendor.mk
    they will be replaced by generic.mk, phone.mk,
    tablet.mk, $(feature).mk.
    
    Bug: 295259752
    Test: presubmit
    Change-Id: If741c412c688e416d1cd70481217f9e69b17a211
    Signed-off-by: Roman Kiryanov <rkir@google.com>

--- product/phone.mk
+++ product/phone.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2012 The Android Open Source Project
+# Copyright (C) 2023 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,22 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#
-# This file is included by other product makefiles to add all the
-# emulator-related modules to PRODUCT_PACKAGES.
-#
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
-ifneq ($(EMULATOR_DISABLE_RADIO),true)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
-endif
-
-ifeq ($(EMULATOR_DISABLE_RADIO),true)
-DEVICE_PACKAGE_OVERLAYS += device/generic/goldfish/tablet/overlay
-else
-DEVICE_PACKAGE_OVERLAYS := device/generic/goldfish/overlay
-endif
-
-PRODUCT_CHARACTERISTICS := emulator
+# All phone specific stuff should be moved here.
 
 $(call inherit-product, device/generic/goldfish/product/generic.mk)
