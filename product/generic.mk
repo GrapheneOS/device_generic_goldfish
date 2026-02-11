a681c0cb2ec2122660200497e0c5ccdf844e78c7
Haitao Shan <hshan@google.com>
2026-02-11 13:44:22 -0800

    Revert^2 "Force PRODUCT_ENABLE_UFFD_GC to true on Goldfish."
    This reverts commit b052c6776260d3785a78c236905ea93e52456603.
    
    Reason for revert: 
    According to my test, reverting the original CL is not equal to setting PRODUCT_ENABLE_UFFD_GC to false. Reverting the CL does not solve the issue of 16K system image. What's more, I constantly had much longer boot time after the revert. Before we can explain what happened, it is better to bring back the original CL.
    
    Change-Id: I5a90ec3117abe765db374e7b003222d4aa4f25b2

--- product/generic.mk
+++ product/generic.mk
@@ -351,8 +351,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:${TARGET_COPY_OUT_PRODUCT}/etc/permissions/android.software.verified_boot.xml \
     device/generic/goldfish/data/etc/permissions/privapp-permissions-goldfish.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-goldfish.xml \
 
-# change to false to make it faster on 16k image
-PRODUCT_ENABLE_UFFD_GC := false
+# Goldfish uses 6.X kernels.
+PRODUCT_ENABLE_UFFD_GC := true
 
 PRODUCT_ENFORCE_SELINUX_TREBLE_LABELING := true
 
